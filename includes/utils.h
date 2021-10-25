e335971f277f83681249ce6a7e4cf2425ade1fd1
leakt <e.bardik96@gmail.com>
2021-10-25 19:53:31 +0300

    Some changes and handles

--- includes/utils.h
+++ includes/utils.h
@@ -3,9 +3,12 @@
 unsigned long long MAX_RAND;
 short multiplier;
 char* file_name;
+enum algorithms { SHELL_SORT = 0, INSERTION_SORT = 1 };
+
+int* (*alg_fs[2])(int*);
 
 int is_a_num(char* str, int length); // check is str a number
-int* is_an_algo(char* str, int length); // check is str a valid algo
+int* (*(return_algo(char* str))) (int*); // check is str a valid algo
 
 int* get_random_array(int size); // get array of a random numbers
 
--- utils.c
+++ utils.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
 
 #include "includes/utils.h"
 
 extern unsigned long long MAX_RAND;
 extern short multiplier;
+extern int* (*alg_fs[2])(int*);
 
 int is_a_num(char* str, int length) {
 
@@ -24,6 +26,7 @@ int is_a_num(char* str, int length) {
 
 	if (res > 0)
 		return res;
+
 	else
 		return 0;
 }
@@ -32,23 +35,36 @@ int* get_random_array(int size) {
 
 	int* arr = (int*)malloc(sizeof(int) * size);
 
-	srand(time(NULL));
+	srand(time(0));
 
-	for (int i = 0; i < size; i++)
+	if (!MAX_RAND)
+		MAX_RAND = RAND_MAX;
+
+	for (int i = 0; i < size; i++) {
+
 		arr[i] = rand() % MAX_RAND;
+		
+	}
 
 	return arr;
 }
 
-int* is_an_algo(char* str, int length) {
+int* (*(return_algo(char* str))) (int*) {
 
+	printf("%p\n", alg_fs[SHELL_SORT]);
+	printf("%p\n", alg_fs[INSERTION_SORT]);
+
+	if (!strcmp(str, "shell_sort") || !strcmp(str, "shell") || !strcmp(str, "shellsort"))
+		return alg_fs[SHELL_SORT];
+
+	if(!strcmp(str, "insertion_sort") || !strcmp(str, "insertion") || !strcmp(str, "insertionsort"))
+		return alg_fs[INSERTION_SORT];
 
 	return NULL;
-
 }
 
 
-void check() {
+void check() { // tmp function
 
 	printf("%d\n", multiplier);
 
